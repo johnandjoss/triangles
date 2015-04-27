e95a9a76a4ad70fd5c06096be8bbe369722efc7c
Jeanette Fairless & Jocelyn Alsdorf <student@epicodus.com>
2015-04-27 16:25:38 -0700

    logic for not_triangle method now producing accurate results

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,13 +13,13 @@ class Triangle
   end
   define_method(:isosceles?) do
     if @side1 == @side2
-      if @side1 != @side3 && @side2 !=@side3
+      if @side1 != @side3 && @side2 != @side3
       true
       else
       false
     end
   elsif  @side2 == @side3
-      if @side2 != @side1 && @side3 !=@side1
+      if @side2 != @side1 && @side3 != @side1
       true
       else
       false
@@ -42,11 +42,11 @@ class Triangle
     end
   end
   define_method(:not_triangle?) do
-    if @side1 >= @side2.+(@side3)
+    if @side1.to_i >= @side2.to_i.+(@side3.to_i)
       true
-    elsif @side2 >= @side3.+(@side1)
+    elsif @side2.to_i >= @side3.to_i.+(@side1.to_i)
       true
-  elsif @side3 >= @side1.+(@side2)
+  elsif @side3.to_i >= @side1.to_i.+(@side2.to_i)
       true
     else
       false
